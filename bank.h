6706711fd8bb850599429b13d70c6504ce387296
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-06-15 21:38:44 +0000

    addedfile

--- bank.h
+++ bank.h
@@ -6,4 +6,5 @@ int transfer(char* password, char* firstname, char* lastname, double balance);
 void deposit(double balance);
 int reciept(char* bankNam, char* firstname, char* lastname, long int acctNum, double balance, long int amount, double current_bal, char* acctType);
 void checBal(double current_bal);
+int validate_pass(char* password, char* confirm_pass);
 #endif
